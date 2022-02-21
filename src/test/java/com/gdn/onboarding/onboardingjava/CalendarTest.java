31b460c6c7cdc78cd340f11c2d34026335fd4a53
Joshua Erlangga <57672096+joshuagatizz@users.noreply.github.com>
2022-02-21 17:28:45 +0700

    Added success tests for simple java

--- src/test/java/com/gdn/onboarding/onboardingjava/CalendarTest.java
+++ src/test/java/com/gdn/onboarding/onboardingjava/CalendarTest.java
@@ -3,6 +3,13 @@ package com.gdn.onboarding.onboardingjava;
 import org.junit.jupiter.api.Test;
 
 public class CalendarTest {
-
+    @Test
+    public void testGetMonth_success() throws Exception{
+        String tc = new Calendar().getMonth(10);
+        if (!tc.equals("October")) {
+            System.out.println("GAGAL");
+            throw new Exception();
+        }
+    }
 
 }
--- src/test/java/com/gdn/onboarding/onboardingjava/FactorialTest.java
+++ src/test/java/com/gdn/onboarding/onboardingjava/FactorialTest.java
@@ -1,4 +1,14 @@
 package com.gdn.onboarding.onboardingjava;
 
+import org.junit.jupiter.api.Test;
+
 public class FactorialTest {
+    @Test
+    public void testFact_success() throws Exception{
+        Integer tc = new Factorial().fact(4);
+        if (tc != 24) {
+            System.out.println("GAGAL");
+            throw new Exception();
+        }
+    }
 }
